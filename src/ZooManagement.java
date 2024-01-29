bd3caeab4de7fb77da9c917d9e3d8b825fbdb589
azizmaitig <azizmaitig7@gmail.com>
2024-01-29 11:05:09 +0100

    first commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,14 @@
+public class ZooManagement {
+}
+
+    public static <string> void main(string[] args) {
+        int nbrCage = 20;
+        string ZooName = "my zoo";
+        Scanner sc = new scanner(system.in);
+        System.out.println(" give me number of cages ");
+        nbrCage = sc.nextInt();
+        System.out.println(" give me zoo name ");
+        nbrCage = sc.nextLine();
+        System.out.println(ZooName + " comporte " + nbrCage + " cages ");
+    }
+}
